96e1950a156885ca0db091c2f45cff42d36b8ef2
SkymanSky <gokera@ogr.iu.edu.tr>
2021-10-19 00:02:15 +0300

    18.10

---
+++ src/app.js
@@ -0,0 +1,33 @@
+console.log("Merhaba kodlama.io")
+
+const name="goker"
+
+
+let dolarBugun = 9.30
+let dolarDun = 9.20
+
+//let dolarDun = "9.20"
+
+{
+    //let dolarDun = 9.10
+}
+
+
+console.log(dolarDun)
+
+const euorDun = 11.2
+//euorDun = 11
+
+console.log(euorDun)
+
+let konutKredileri = ["Konut Kredisi","Emlak konut kredisi","Kamu konut kredisi"]
+
+konutKredileri.push("Polislere Özel")
+
+//console.log(konutKredileri)
+
+for (let index = 0; index < konutKredileri.length; index++) {
+    const element = konutKredileri[index];
+    console.log(element)
+}
+
